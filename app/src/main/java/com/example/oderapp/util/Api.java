befaf16fff2feee8b77e2f7de2184d288d18e296
kien2811 <kienphama2ht@gmail.con>
2021-01-06 20:04:24 +0700

    kien

--- app/src/main/java/com/example/oderapp/util/Api.java
+++ app/src/main/java/com/example/oderapp/util/Api.java
@@ -1,7 +1,7 @@
 package com.example.oderapp.util;
 
 public class Api {
-    public static final  String HOST = "192.168.1.11:8888/oder_cart_php/public";
+    public static final  String HOST = "192.168.1.3:8888/oder_cart_php/public";
 
     public static final  String URl_SLIDER = "http://"+HOST+"/?controller=index&action=slider";
     public static final  String URl_SLIDER_PRODUCT = "http://"+HOST+"/?controller=index&action=Select_Slider_Product&id_product=";
@@ -12,6 +12,7 @@ public class Api {
     public static final  String URL_IMG_PROFILE = "http://"+HOST+"/";
     public static final  String URl_REGISTER = "http://"+HOST+"/?controller=index&action=register";
     public static final  String URl_CHANGE_PROFILE = "http://"+HOST+"/?controller=index&action=change_profile";
+    public static final  String URl_CHECK_ADDRESS = "http://"+HOST+"/?controller=index&action=check_address";
     public static final  String URl_UPLOAD_IMAGE = "http://"+HOST+"/?controller=index&action=user_profile&token=";
     public static final  String URl_LOGIN = "http://"+HOST+"/";
     public static final  String URL_ALL_PRODUCT = "http://"+HOST+"/?controller=index&action=all_product&page=";
